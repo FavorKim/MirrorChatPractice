a99eecbb4754d4ec6f0081b81a0a936110c33cab
FavorKim <harangs99@daum.net>
2024-05-29 11:53:44 +0900

    [feat] 로그인 팝업 이벤트 등록

--- Assets/MirrorChat/Scripts/LoginPopup.cs
+++ Assets/MirrorChat/Scripts/LoginPopup.cs
@@ -14,4 +14,61 @@ public class LoginPopup : MonoBehaviour
 
     [SerializeField] private Text Text_Error;
 
+    
+    private string m_originNetworkAddress;
+
+    // m_ << 이거 왜 붙임? -> 나는 전역변수 입니다를 알려주기 위해.
+    // 전역변수는 지역변수와 다르게 클래스 내 어디서든 접근하고 수정이 가능하므로
+
+    private void Awake()
+    {
+        
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    /*
+    클릭 - 호스트로 시작
+    클릭 - 클라로 시작
+    Input - 서버 주소 입력 완료
+    Input - 서버 주소 입력 중
+    
+    Input - 유저 닉네임 입력 완료 (OnSubmit) << 이벤트에는 왜 On이 붙는가? -> 수동적인 녀석 즉, 누군가에 의해서만 호출되는 함수
+    Input - 유저 닉네임 입력 중 (OnValueChanged)
+    */
+
+    public void OnClick_StartAsHostServer()
+    {
+
+    }
+
+    public void OnClick_StartAsClient()
+    {
+
+    }
+
+    public void OnValueChanged_ToggleBtn(string userName)
+    {
+
+    }
+
+    
 }
